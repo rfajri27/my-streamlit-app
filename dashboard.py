a63b20f37d41e2fea5366f892706480fe61d43b7
rfajri27 <rfajri255@gmail.com>
2023-03-29 14:09:24 +0700

    update

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 
 # Load cleaned data
 sum_order_items_df = pd.read_csv("sum_order_items.csv")
